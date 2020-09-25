1385427cc44b8b3cbbc230ca6812f3a97c88b54e
Subrat Kumar Padhi <spadhi2@yodlee.com>
2020-09-25 17:05:18 +0530

    Updates

---
+++ precache-manifest.ac33d6ea91a770e14f037783228121ab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5005c48fbbc2e566ac89ff087daca9ce",
+    "url": "/sales-dashboard/index.html"
+  },
+  {
+    "revision": "cc189b2e21cf319053db",
+    "url": "/sales-dashboard/static/css/2.305e1ae7.chunk.css"
+  },
+  {
+    "revision": "ed0a41d4f5f41996c7e2",
+    "url": "/sales-dashboard/static/css/main.a3efcb2c.chunk.css"
+  },
+  {
+    "revision": "cc189b2e21cf319053db",
+    "url": "/sales-dashboard/static/js/2.ce474232.chunk.js"
+  },
+  {
+    "revision": "79b8af32e3a2fb7032ceedc6fc8a95b6",
+    "url": "/sales-dashboard/static/js/2.ce474232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0a41d4f5f41996c7e2",
+    "url": "/sales-dashboard/static/js/main.6cd3683f.chunk.js"
+  },
+  {
+    "revision": "88b0634797ea91314ef3",
+    "url": "/sales-dashboard/static/js/runtime-main.a22619f7.js"
+  },
+  {
+    "revision": "46a206c728ba27c414c7a15a774ab482",
+    "url": "/sales-dashboard/static/media/12.46a206c7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sales-dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
